427c8757851e0ed6e539dc13b9660e34f73931b4
Eldan Vaknin <eldan@appdome.com>
2024-04-07 15:11:54 +0300

    modify the subprocess running, modify venv activation for windows

--- setup_venv.sh
+++ setup_venv.sh
@@ -13,15 +13,24 @@ activate_virtual_env() {
         echo "Using existing virtual environment '$venv_name'."
     fi
 
-    source "$venv_name/bin/activate"
-
-    if [ -f "$requirements_file" ]; then
-        pip3 install -r "$requirements_file"
-        echo "Requirements installed from '$requirements_file'."
+    if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+        # Unix/Linux/macOS
+        source "$venv_name/bin/activate"
+    elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "win32"* ]]; then
+        # Windows
+        source "$venv_name/Scripts/activate"
     else
-        echo "Requirements file '$requirements_file' not found."
+        echo "Unsupported OS."
         exit 1
     fi
+
+#    if [ -f "$requirements_file" ]; then
+#        pip3 install -r "$requirements_file"
+#        echo "Requirements installed from '$requirements_file'."
+#    else
+#        echo "Requirements file '$requirements_file' not found."
+#        exit 1
+#    fi
 }
 
 activate_virtual_env
